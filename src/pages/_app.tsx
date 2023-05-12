e94f06e9fe12eae6f618b73f35fd113c9ffdd252
Frank Luan <lsf@berkeley.edu>
2023-05-12 07:11:52 +0000

    feat(site): add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,33 @@
 import "../styles/global.css";
 
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { SnackbarProvider } from "notistack";
 import { RecoilRoot } from "recoil";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <SnackbarProvider>
-        <Component {...pageProps} />
-      </SnackbarProvider>
-    </RecoilRoot>
+    <>
+      <RecoilRoot>
+        <SnackbarProvider>
+          <Component {...pageProps} />
+        </SnackbarProvider>
+      </RecoilRoot>
+
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-EDYBRW20MH"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-EDYBRW20MH');
+        `}
+      </Script>
+    </>
   );
 }
