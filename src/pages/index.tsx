ccef66010202e9ee34f1ce8695868a135e20ce44
Frank Luan <lsf@berkeley.edu>
2023-05-14 06:31:48 +0000

    fix(index): use dynamic loading for index sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
+import dynamic from "next/dynamic";
+
 import { AboutSection } from "@/components/index/AboutSection";
 import { CatalogSection } from "@/components/index/CatalogSection";
 import { CustomSourcingSection } from "@/components/index/CustomSourcingSection";
-import { FoundingTeamSection } from "@/components/index/FoundingTeamSection";
 import { HeroSection } from "@/components/index/HeroSection";
 import { OurPromiseSection } from "@/components/index/OurPromiseSection";
 import { AppLayout } from "@/layouts/AppLayout";
 import { Meta } from "@/layouts/Meta";
 import { AppConfig } from "@/utils/AppConfig";
 
-export default function Index() {
+const FoundingTeamSection = dynamic(() =>
+  import("@/components/index/FoundingTeamSection").then(
+    (mod) => mod.FoundingTeamSection
+  )
+);
+
+export default function IndexPage() {
   return (
     <AppLayout
       meta={
