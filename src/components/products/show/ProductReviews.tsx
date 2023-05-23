879f2f910067dc8e6d35312d6257c6486737eb7b
Frank Luan <lsf@berkeley.edu>
2023-05-23 02:34:02 +0000

    fix(product): review stars should be solid color

--- src/components/products/show/ProductReviews.tsx
+++ src/components/products/show/ProductReviews.tsx
@@ -1,4 +1,4 @@
-import { StarIcon } from "@heroicons/react/24/outline";
+import { StarIcon } from "@heroicons/react/24/solid";
 import classNames from "classnames";
 
 export function ProductReviews({ rating }: { rating: number }) {
