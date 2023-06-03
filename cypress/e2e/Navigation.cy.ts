4f4be998957ef5500a4458061f369fd98ba65a16
Frank Luan <lsf@berkeley.edu>
2023-06-03 21:36:56 +0000

    build: fix cypress

--- cypress/e2e/Navigation.cy.ts
+++ cypress/e2e/Navigation.cy.ts
@@ -3,24 +3,19 @@ describe("Navigation", () => {
     it("should navigate to the about page", () => {
       // Start from the index page
       cy.visit("/");
-      cy.findByRole("heading", {
-        name: "You dream it",
-      });
 
       // Navigate to /custom-request
       cy.findByRole("link", { name: "Custom Request" }).click();
       cy.url().should("include", "/custom-request");
-      cy.findAllByText("White-Glove VIP Service", { exact: false }).should(
+      cy.findAllByText("White-Glove VIP service", { exact: false }).should(
         "have.length",
-        2
+        3
       );
     });
 
     it("should take screenshot of the homepage", () => {
       cy.visit("/");
-      cy.findByRole("heading", {
-        name: "You dream it",
-      });
+      cy.findByRole("link", { name: "Custom Request" });
       cy.percySnapshot("Homepage");
     });
   });
