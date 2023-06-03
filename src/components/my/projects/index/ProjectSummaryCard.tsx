707c36a5da2492d791e1f9ffdd5ecdf36a61421c
Frank Luan <lsf@berkeley.edu>
2023-06-03 23:47:01 +0000

    fix(projects): fix project card style and layout errors

--- src/components/my/projects/index/ProjectSummaryCard.tsx
+++ src/components/my/projects/index/ProjectSummaryCard.tsx
@@ -16,42 +16,47 @@ function ProjectImageView({ project }: { project: Project }) {
       className="mx-auto h-32 w-32 shrink-0"
     />
   ) : (
-    <div className="mx-auto flex h-32 w-32 items-center justify-center text-6xl">
+    <div className="mx-auto flex h-32 w-32 items-center justify-center text-6xl text-gray-700">
       <FaFeatherAlt />
     </div>
   );
 }
 
 export function ProjectSummaryCard({ project }: { project: Project }) {
+  const projectLink = `/my/projects/${project.id}`;
   return (
-    <Link href={`/my/projects/${project.id}`}>
-      <div className="relative flex w-64 flex-col rounded-lg bg-slate-50 text-center shadow">
-        <div className="absolute right-2 top-2 text-gray-300">
+    <div className="relative flex w-64 flex-col rounded-lg bg-slate-50 text-center shadow">
+      <div className="absolute right-2 top-2 text-gray-300">
+        <Link href={projectLink}>
           <FaExpandArrowsAlt />
-        </div>
-        <div className="flex flex-1 flex-col p-4 pt-2">
-          <ProjectImageView project={project} />
-          <h3 className="mt-2 font-medium text-gray-900">{project.name}</h3>
-          <ul className="mt-2 flex grow flex-col justify-between">
-            <li>
-              <ProjectStatusBadge project={project} />
-            </li>
-            {project.orders.length === 0 ? (
-              <li className="mt-3 text-sm text-gray-500">No orders yet</li>
-            ) : (
-              <li>
-                <ul className="mt-4 space-y-2">
-                  {project.orders.map((order) => (
-                    <li key={order.id} className="mx-auto">
-                      <OrderBadge order={order} />
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            )}
-          </ul>
-        </div>
+        </Link>
       </div>
-    </Link>
+      <div className="flex flex-1 flex-col p-4">
+        <Link href={projectLink}>
+          <ProjectImageView project={project} />
+        </Link>
+        <h3 className="mt-2 font-medium text-gray-900">
+          <Link href={projectLink}>{project.name}</Link>
+        </h3>
+        <ul className="mt-2 flex grow flex-col justify-between">
+          <li>
+            <ProjectStatusBadge project={project} />
+          </li>
+          {project.orders.length === 0 ? (
+            <li className="mt-3 text-sm text-gray-500">No orders yet</li>
+          ) : (
+            <li>
+              <ul className="mt-4 space-y-2">
+                {project.orders.map((order) => (
+                  <li key={order.id} className="mx-auto">
+                    <OrderBadge order={order} />
+                  </li>
+                ))}
+              </ul>
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
   );
 }
