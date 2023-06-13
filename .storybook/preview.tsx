c959704f59a6f3614d7296b41e3bc1edfdc42aaf
Frank Luan <lsf@berkeley.edu>
2023-06-13 22:24:05 +0000

    test(storybook): fix storybook setup for Recoil-enabled components

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,17 @@
 import "../src/styles/global.css";
 
 import type { Preview } from "@storybook/react";
+import React from "react";
+import { RecoilRoot } from "recoil";
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <RecoilRoot>
+        <Story />
+      </RecoilRoot>
+    ),
+  ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
--- src/components/my/projects/ContentVisibilityBanner.stories.tsx
+++ src/components/my/projects/ContentVisibilityBanner.stories.tsx
@@ -1,18 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { RecoilRoot } from "recoil";
 
 import { ContentVisibilityBanner } from "./ContentVisibilityBanner";
 
 const meta: Meta<typeof ContentVisibilityBanner> = {
   component: ContentVisibilityBanner,
   tags: ["autodocs"],
-  decorators: [
-    (Story) => (
-      <RecoilRoot>
-        <Story />
-      </RecoilRoot>
-    ),
-  ],
 };
 
 export default meta;
